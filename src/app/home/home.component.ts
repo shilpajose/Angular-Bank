18e2d634a890eee9a9e7507a9bdcc9e66b329006
Shilpa Midhun <shilpajm05@gmail.com>
2023-09-07 10:19:01 +0530

    bank-backend- login function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,13 +10,10 @@ export class HomeComponent {
 
   data = "Happy Banking with us"
 
-  sdata:any
+ 
   constructor(private ds:DataService){}
 
   ngOnInit():void{
-    setTimeout(()=>{
-      this.ds.serviceMethod()
-    },2000)
-    this.sdata=this.data
+   
   }
 }
